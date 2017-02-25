ed87fa7f6f262fb2510cf632ada95ecb25922d4f
Tkc <loosefingers@gmail.com>
2017-02-25 17:47:22 +0900

    Change message text

--- app/requestMessages.go
+++ app/requestMessages.go
@@ -6,3 +6,7 @@ const ImageMessage = "ImageMessage"
 const CarouselMessage = "carouselMessage"
 const MoreSelect = "moreSelectlMessage"
 
+const CarouselMessageSelectA = "CarouselMessageSelectA"
+const CarouselMessageSelectB = "CarouselMessageSelectB"
+const CarouselMessageSelectC = "CarouselMessageSelectC"
+const CarouselMessageSelectD = "CarouselMessageSelectD"
